1bf1fffa0b0835771cde920bddfb11f94f088d98
BasinS <shoom.pozitiv@gmail.com>
2020-11-10 21:00:41 +0200

    Initial Commit

---
+++ NetworkApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkApp
+//
+//  Created by Sergey Basin on 10.11.2020.
+//  Copyright © 2020 Sergey Basin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
